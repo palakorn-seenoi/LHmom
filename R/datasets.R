b88780adebc8ed8f997fcc4fa8e97637ccd20701
palakorn-seenoi <palakorns@kku.ac.th>
2026-06-13 00:11:51 +0700

    Create datasets.R

---
+++ R/datasets.R
@@ -0,0 +1,103 @@
+#' Annual Maximum Daily Rainfall for Bangkok
+#'
+#' A dataset containing the annual maximum series (AMS) of daily rainfall
+#' measurements in Bangkok, Thailand, from 1951 to 2025. This dataset is
+#' typically used for extreme value analysis and hydrological frequency modeling.
+#'
+#' @format A data frame with 75 rows and 2 variables:
+#' \describe{
+#'   \item{year}{The observation year (1951-2025).}
+#'   \item{max_rain}{The annual maximum daily rainfall in millimeters (mm).}
+#' }
+#' @source Thai Meteorological Department (TMD)
+#' @usage data(rain_bangkok)
+#' @name rain_bangkok
+#' @docType data
+NULL
+
+
+
+#' Annual Maximum Daily Rainfall for Khon Kaen
+#'
+#' A dataset containing the annual maximum series (AMS) of daily rainfall
+#' measurements in Khon Kaen, Thailand, from 1984 to 2025.
+#'
+#' @format A data frame with 42 rows and 2 variables:
+#' \describe{
+#'   \item{year}{The observation year (1984-2025).}
+#'   \item{max_rain}{The annual maximum daily rainfall in millimeters (mm).}
+#' }
+#' @source Thai Meteorological Department (TMD)
+#' @usage data(rain_khonkaen)
+#' @name rain_khonkaen
+#' @docType data
+NULL
+
+
+#' Annual Maximum Daily Rainfall for Maha Sarakham
+#'
+#' A dataset containing the annual maximum series (AMS) of daily rainfall
+#' measurements in Maha Sarakham, Thailand, from 1984 to 2025.
+#'
+#' @format A data frame with 42 rows and 2 variables:
+#' \describe{
+#'   \item{year}{The observation year (1984-2025).}
+#'   \item{max_rain}{The annual maximum daily rainfall in millimeters (mm).}
+#' }
+#' @source Thai Meteorological Department (TMD)
+#' @usage data(rain_mahasarakham)
+#' @name rain_mahasarakham
+#' @docType data
+NULL
+
+
+#' Annual Maximum Daily Rainfall for Tha Phra, Khon Kaen
+#'
+#' A dataset containing the annual maximum series (AMS) of daily rainfall
+#' measurements at the Tha Phra station in Khon Kaen, Thailand, from 1984 to 2025.
+#'
+#' @format A data frame with 42 rows and 2 variables:
+#' \describe{
+#'   \item{year}{The observation year (1984-2025).}
+#'   \item{max_rain}{The annual maximum daily rainfall in millimeters (mm).}
+#' }
+#' @source Thai Meteorological Department (TMD)
+#' @usage data(rain_thaphra)
+#' @name rain_thaphra
+#' @docType data
+NULL
+
+
+#' Annual Maximum Daily Temperature for Khon Kaen
+#'
+#' A dataset containing the annual maximum series (AMS) of daily temperature
+#' measurements in Khon Kaen, Thailand, from 1985 to 2025. This dataset is
+#' useful for modeling extreme heat events.
+#'
+#' @format A data frame with 42 rows and 2 variables:
+#' \describe{
+#'   \item{year}{The observation year (1985-2025).}
+#'   \item{max_temp}{The annual maximum daily temperature in degrees Celsius (°C).}
+#' }
+#' @source Thai Meteorological Department (TMD)
+#' @usage data(temp_khonkaen)
+#' @name temp_khonkaen
+#' @docType data
+NULL
+
+
+#' Annual Maximum Daily Temperature for Maha Sarakham
+#'
+#' A dataset containing the annual maximum series (AMS) of daily temperature
+#' measurements in Maha Sarakham, Thailand, from 1985 to 2025.
+#'
+#' @format A data frame with 41 rows and 2 variables:
+#' \describe{
+#'   \item{year}{The observation year (1985-2025).}
+#'   \item{Tmax}{The annual maximum daily temperature in degrees Celsius (°C).}
+#' }
+#' @source Thai Meteorological Department (TMD)
+#' @usage data(temp_mahasarakham)
+#' @name temp_mahasarakham
+#' @docType data
+NULL
